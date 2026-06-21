eaa4dbbe854056d648b35a41097d734ddee2ead4
Jackson O. <jmo.dev97@gmail.com>
2026-06-21 03:02:15 +0000

    chore: reestrutura o projeto separando frontend e backend em pastas

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Cria uma instância do Axios com o URL base do teu backend em Node.js
 export const api = axios.create({
-  baseURL: "https://silver-cod-69pg7xw4jwxphwwg-3000.app.github.dev/", // Certifica-te de que o teu backend corre nesta porta
+  baseURL: "https://bookish-space-funicular-v65v7jwqxr4pfxv77-3000.app.github.dev/", // Certifica-te de que o teu backend corre nesta porta
 });
 
 // Intercetor para injetar o token JWT em todas as requisições automaticamente
